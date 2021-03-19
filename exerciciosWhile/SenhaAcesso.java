2bb25bf22afabff668dc27083118a8cf9f814117
Geisson De Lima Silva <75763408+Geisson-Silva@users.noreply.github.com>
2021-03-19 15:21:05 -0300

    Add files via upload

---
+++ exerciciosWhile/SenhaAcesso.java
@@ -0,0 +1,22 @@
+package exerciciosWhile;
+
+import java.util.Scanner;
+
+public class SenhaAcesso {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while (senha!= 2002) {
+			System.out.println("SENHA INVÁLIDA");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("ACESSO PERMITIDO");
+		
+		sc.close();
+	}
+
+}
